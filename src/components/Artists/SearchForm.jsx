7ad6a98c7466db643b7991d8de466427cf3da24d
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-04 16:30:17 -0700

    add SearchForm component

---
+++ src/components/Artists/SearchForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchForm = ({ searchQuery, onInputChange, onSearch }) => (
+  <section>
+    <form onSubmit={onSearch}>
+      <input value={searchQuery} onChange={onInputChange} />
+      <button>Search</button>
+    </form>
+  </section>
+);
+
+SearchForm.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired
+};
