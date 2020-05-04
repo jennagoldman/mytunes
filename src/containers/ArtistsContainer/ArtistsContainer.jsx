41b0aab2926ee697005af3c58008d4faebc472a6
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-04 16:11:52 -0700

    add state, callback functions and presentation component setup to ArtistsContainer

--- src/containers/ArtistsContainer/ArtistsContainer.jsx
+++ src/containers/ArtistsContainer/ArtistsContainer.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchForm from '';
+import ArtistsList from '';
+import Paging from '';
 
 const ArtistsContainer = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [artists, setArtists] = useState([]);
+  
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchArtists(searchQuery)
+      .then(artists => setArtists(artists));
+  };
+
+  const handleInput = ({ target }) => {
+    setSearchQuery(target.value);
+  };
 
   return (
+    <>
+      <SearchForm onInputChange={handleInput} onSubmit={handleSearch} />
+      <ArtistsList artists={artists} />
+      <Paging />
+    </>
+  );
+};
 
-  )
-}
+export default ArtistsContainer;
