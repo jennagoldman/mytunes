8348b9fa25f9f3715c73a14241980d6511346850
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-05 15:12:54 -0700

    release title and cover displaying in ArtistReleases

--- src/components/Releases/ArtistReleases.jsx
+++ src/components/Releases/ArtistReleases.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ArtistReleases = ({ artist, releases }) => (
-  <>
-    <h2>{artist.name} - Release History</h2>
-  </>
-);
+const ArtistReleases = ({ artist, releases }) => {
+  const releasesListItems = releases.map(release => (
+    <li key={artist.id}>
+      <Link to={`/release/${release.id}`}>
+        <h3>{release.title}</h3>
+        <img src={`http://coverartarchive.org/release/${release.id}/front`} alt={release.title} />
+      </Link>
+    </li>
+  ));
+
+  return (
+    <>
+      <h2>{artist.name} - Release History</h2>
+      <ul>
+        {releasesListItems}
+      </ul>
+    </>
+  );
+};
 
 ArtistReleases.propTypes = {
   artist: PropTypes.object.isRequired,
